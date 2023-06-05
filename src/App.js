299a9cb33a60b4480a04bcd176c924aba51144ac
Taner <taneozer16@gmail.com>
2023-06-06 00:49:20 +0300

    appjs and appcss added

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AllCountries from "./components/AllCountries/AllCountries";
+import CountryInfo from "./components/CountryInfo/CountryInfo";
+
+function App() {
+  return (
+    <Router>
+      <div className="header">
+        <div className="container">
+          <h5>Where in the world</h5>
+        </div>
+      </div>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<AllCountries />} />
+          <Route path="/country/:countryName" element={<CountryInfo />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
