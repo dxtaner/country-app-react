527bbe9b1b1d1756d29578a153b5ab31ecf4ae5b
Taner <taneozer16@gmail.com>
2023-06-07 20:14:40 +0300

    CountryInfo added

---
+++ src/components/CountryInfo/CountryInfo.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { apiURL } from "../Utilities/GetApi.jsx";
+
+const CountryInfo = () => {
+  const [country, setCountry] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const { countryName } = useParams();
+
+  useEffect(() => {
+    const getCountryByName = async () => {
+      try {
+        const res = await fetch(`${apiURL}/name/${countryName}`);
+
+        if (!res.ok) throw new Error("Country not found!");
+
+        const data = await res.json();
+
+        setCountry(data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setError(error.message);
+      }
+    };
+
+    getCountryByName();
+  }, [countryName]);
+
+  return (
+    <div className="countryInfoWrapper">
+      <button>
+        <Link to="/">Back</Link>
+      </button>
+
+      {isLoading && !error && <h4>Loading........</h4>}
+      {error && !isLoading && <h4>{error}</h4>}
+
+      {country?.map((country, index) => (
+        <div className="countryInfoContainer" key={index}>
+          <div className="countryInfoImg">
+            <img src={country.flags.png} alt="" />
+          </div>
+
+          <div className="countryInfo">
+            <h3>{country.name.common}</h3>
+
+            <div className="countryInfoLeft">
+              <h5>
+                Population:{" "}
+                <span>
+                  {new Intl.NumberFormat().format(country.population)}
+                </span>
+              </h5>
+              <h5>
+                Region: <span>{country.region}</span>
+              </h5>
+              <h5>
+                Sub Region: <span>{country.subregion}</span>
+              </h5>
+              <h5>
+                Capital: <span>{country.capital}</span>
+              </h5>
+              <h5>
+                Maps: <span>{country.maps.openStreetMaps}</span>
+              </h5>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CountryInfo;
