7326e04b4776c0e6e8e1cac41bdb5d450457f824
Taner <taneozer16@gmail.com>
2023-06-07 20:24:30 +0300

    SearchInput added

---
+++ src/components/Search/SearchInput.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const SearchInput = ({ onSearch }) => {
+  const [input, setInput] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    onSearch(input);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        placeholder="Search a country..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchInput;
