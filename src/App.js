01f452895ab6c9bce2b3fd53bf6b20027b68a8f6
Taner <taneozer16@gmail.com>
2023-06-08 19:50:38 +0300

    edited css

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import CountryInfo from "./components/CountryInfo/CountryInfo";
 function App() {
   return (
     <Router>
-      <div className="header">
+      <header className="header">
         <div className="container">
-          <h5>Where in the world</h5>
+          <h5>Where in the world?</h5>
         </div>
-      </div>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<AllCountries />} />
-          <Route path="/country/:countryName" element={<CountryInfo />} />
-        </Routes>
-      </div>
+        
+      </header>
+      <main className="Main">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<AllCountries />} />
+            <Route path="/country/:countryName" element={<CountryInfo />} />
+          </Routes>
+        </div>
+      </main>
     </Router>
   );
 }
